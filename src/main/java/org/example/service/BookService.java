71daaf367b0b5a0ef241a2543f2719c9d5eedc0c
Alex Braga <contato@alexbraga.com.br>
2024-06-06 23:10:17 -0300

    Implement delete book functionality in `BookService` and refactor the code
    The code refactoring consisted in removing repeated calls to 'BOOK_DAO.findAll', creating a 'bookList' field and initializing it in the 'BookService' constructor, keeping a single copy that's always in sync and reducing the number of disk operations

--- src/main/java/org/example/service/BookService.java
+++ src/main/java/org/example/service/BookService.java
@@ -8,20 +8,20 @@ import java.util.Optional;
 
 public class BookService {
     private final BookDAO BOOK_DAO;
+    private final List<Book> BOOK_LIST;
 
     public BookService(BookDAO bookDAO) {
         this.BOOK_DAO = bookDAO;
+        this.BOOK_LIST = BOOK_DAO.findAll();
     }
 
     public List<Book> findAllAndIndex() {
-        List<Book> bookList = BOOK_DAO.findAll();
-
-        for (Book book : bookList) {
+        for (Book book : BOOK_LIST) {
             IndexService index = IndexService.getInstance();
             index.addToIndex(book);
         }
 
-        return bookList;
+        return BOOK_LIST;
     }
 
     public void saveAllAndIndex(List<Book> bookList) {
@@ -45,14 +45,13 @@ public class BookService {
 
     public Book update(String isbn, Book updatedBook) {
         Optional<Book> bookOptional = findByISBN(isbn);
-        List<Book> bookList = BOOK_DAO.findAll();
 
         if (bookOptional.isPresent()) {
             Book book = bookOptional.get();
-            int bookIndex = bookList.indexOf(book);
-            bookList.set(bookIndex, updatedBook);
+            int bookIndex = BOOK_LIST.indexOf(book);
+            BOOK_LIST.set(bookIndex, updatedBook);
 
-            BOOK_DAO.saveAll(bookList);
+            BOOK_DAO.saveAll(BOOK_LIST);
 
             IndexService index = IndexService.getInstance();
             index.removeFromIndex(book);
@@ -61,6 +60,25 @@ public class BookService {
             return updatedBook;
         } else {
             throw new BookNotFoundException("Book not found");
+        }
+    }
+
+    public void delete(String isbn) {
+        Optional<Book> bookOptional = findByISBN(isbn);
+
+        if (bookOptional.isPresent()) {
+            Book book = bookOptional.get();
+            int bookIndex = BOOK_LIST.indexOf(book);
+            BOOK_LIST.remove(bookIndex);
+
+            BOOK_DAO.saveAll(BOOK_LIST);
+
+            IndexService index = IndexService.getInstance();
+            index.removeFromIndex(book);
+
+            System.out.println("Book deleted successfully");
+        } else {
+            System.out.println("Book not found");
         }
     }
 }
